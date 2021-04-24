726b48eda846381aff6ebedffd51c5a02d6e9732
Aramantos <john.doyle.mail@icloud.com>
2021-04-24 12:46:45 +0000

    added sql

--- test.sql
+++ test.sql
@@ -1,5 +1,5 @@
-select count(*) from Track;
-select count(*) from Artist;
-select count(*) from Invoice;
-select count(*) from Playlist;
-select count(*) from PlaylistTrack;
+select count (*) from Track;
+select count (*) from Artist;
+select count (*) from Invoice;
+select count (*) from Playlist;
+select count (*) from PlaylistTrack;
